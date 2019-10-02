4edd0a34e2ff13279076b8275cb5fde839ec94d7
Aniket <anikets160@gmail.com>
2019-10-02 12:54:21 +0530

    TestProject1
    TestProject1

---
+++ TestProject1/TestProject1/Program.cs
@@ -0,0 +1,21 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProject1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://www.google.co.in");
+            driver.Quit();
+
+        }
+    }
+}
---
+++ TestProject1/TestProject1/Tests/GoogleTest.cs
@@ -0,0 +1,35 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProject1.Tests
+{
+    public class GoogleTest
+    {
+        IWebDriver driver;
+
+        [SetUp]
+        public void Initialize()
+        {
+            driver = new ChromeDriver();
+        }
+
+        [Test]
+        public void Test()
+        {
+
+            driver.Navigate().GoToUrl("https://www.google.co.in");
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            driver.Quit();
+        }
+    }
+}
